ab555a56b18bc25bccbaf4eb3f35cefc5407871e
Juan Riaza <juanriaza@gmail.com>
2012-03-13 23:44:23 +0100

    asdf

--- verbose/verbose_requests.py
+++ verbose/verbose_requests.py
@@ -1,6 +1,11 @@
 import sys
 import requests
 
-
+# 1
 verbose_config = {'verbose': sys.stderr}
 req = requests.get('http://www.codemotion.es', config=verbose_config)
+
+# 2
+s = requests.session()
+s.config['verbose'] = sys.stderr
+req = s.get('http://www.codemotion.es')
